ad5b61c3fb5053638ea7b180201587f0ae51c694
froches2025 <f.michael@alustudent.com>
2026-06-29 18:04:09 +0200

    task_0: documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,46 @@
 #!/usr/bin/python3
 
+"""
+Script to get and display TODO list progress for an employee
+using a REST API.
+
+Requirements:
+- Use urllib or requests module
+- Accept an integer as a parameter (employee ID)
+- Display progress information in the specified format
+"""
+
 import requests
 import sys
 
 
 def fetch_todo_progress(employee_id: int):
+    """ Fetches and displays TODO list progress for an employee indicated by the given ID.
 
+    Args:
+    - employee_id (int): ID of the employee to retrieve TODO list for.
+
+    Returns:
+    - None
+    """
+    
+    # define core url patterns
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_info_url = f'{base_url}users/{employee_id}'
     todo_info_url = f'{base_url}todos?userId={employee_id}'
 
+    # fetch user information
     user_response = requests.get(user_info_url)
     user_info = user_response.json()
     user_name = user_info.get('name')
 
+    # fetch todo info and calculate totals
     todo_info = requests.get(todo_info_url)
     todo_response = todo_info.json()
     total_todos = len(todo_response)
     completed_todos = sum(todo.get("completed", False) for todo in todo_response)
 
+    # define output
     print(f"Employee {user_name} is done with tasks({completed_todos}/{total_todos}):")
     
     for todo in todo_response:
@@ -27,7 +49,7 @@ def fetch_todo_progress(employee_id: int):
 
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
+    # Checks if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
@@ -35,5 +57,5 @@ if __name__ == "__main__":
     # Extract employee ID from command-line arguments
     employee_id = int(sys.argv[1])
 
-    # Call the function to get and display TODO list progress
+    # Calling the function to get and display TODO list progress
     fetch_todo_progress(employee_id)
