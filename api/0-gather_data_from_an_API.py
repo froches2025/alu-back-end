3849c8676db0b72868839d202791b75a3e85560b
froches2025 <f.michael@alustudent.com>
2026-06-29 18:07:47 +0200

    pep8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,7 +15,8 @@ import sys
 
 
 def fetch_todo_progress(employee_id: int):
-    """ Fetches and displays TODO list progress for an employee indicated by the given ID.
+    """ Fetches and displays TODO list progress for an employee
+    indicated by the given ID.
 
     Args:
     - employee_id (int): ID of the employee to retrieve TODO list for.
@@ -23,7 +24,6 @@ def fetch_todo_progress(employee_id: int):
     Returns:
     - None
     """
-    
     # define core url patterns
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_info_url = f'{base_url}users/{employee_id}'
@@ -38,15 +38,16 @@ def fetch_todo_progress(employee_id: int):
     todo_info = requests.get(todo_info_url)
     todo_response = todo_info.json()
     total_todos = len(todo_response)
-    completed_todos = sum(todo.get("completed", False) for todo in todo_response)
+    completed_todos = sum(todo.get("completed", False)
+                          for todo in todo_response)
 
     # define output
-    print(f"Employee {user_name} is done with tasks({completed_todos}/{total_todos}):")
+    print(f"Employee {user_name} is done with\
+        tasks({completed_todos}/{total_todos}):")
     
     for todo in todo_response:
         if todo.get('completed', False):
-            print(f"{todo.get('title')}")
-
+            print(f"\t {todo.get('title')}")
 
 if __name__ == "__main__":
     # Checks if the correct number of command-line arguments is provided
